6c8bd3d76d80f6e3bd051046f6876ce4f4cd14f8
Lee Taesung <leetsxpro@gmail.com>
2022-06-21 15:31:39 +0900

    2022-06-21 First update

---
+++ README.md
@@ -0,0 +1,15 @@
+# 일프로연산 데이터 흐름
+
+> Spring boot로 개발된 Back end
+
+## 프로젝트 형태
+- Project: Maven Project
+- Language: Java 17
+- Spring Boot: 2.7.0
+- Packaging: Jar
+- Dependencies
+  - Spring Web
+  - MyBatis Framework
+  - MySQL Driver (운영 서버 DB)
+  - MariaDB Driver (개발 서버 DB)
+  - Lombok
---
+++ src/main/java/com/onepromath/DataFlowApplication.java
@@ -0,0 +1,13 @@
+package com.onepromath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataFlowApplication.class, args);
+    }
+
+}
